59457247d36b656b855d2a7e7d21bbbf46c7f329
vapvin <tpehdj12@daum.net>
2019-12-24 15:53:14 +0900

    Class And Static Method

--- fast.js
+++ fast.js
@@ -1,20 +1,9 @@
 'use strict'
 
-let string = 'node.js 올인원 패키지'
-
-let isStartWith = string.startsWith('n')
-
-let isIncludes = string.includes(',')
-
-let isisEndsWith = string.endsWith('지')
-
-
-const checkIfContains = () => {
-    if((isIncludes || isStartWith) && isisEndsWith ) {
-        return true;
+class Test {
+    static call() {
+        console.log("Static Method")
     }
 }
 
-const res = checkIfContains()
-
-console.log(res)
+Test.call()
