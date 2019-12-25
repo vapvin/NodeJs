092fcd9487cd21ab29749e71975dd67caef3f24a
vapvin <tpehdj12@daum.net>
2019-12-25 12:52:06 +0900

    Arrow Functions

--- fast.js
+++ fast.js
@@ -1,17 +1,3 @@
 'use strict'
 
-const test = new Set();
-
-test.add(1)
-test.add(1)
-test.add(2)
-test.add(2)
-test.add(3)
-
-for(const item of test) {
-    console.log(item)
-}
-
-const ret = test.has(0)
-
-console.log(ret)
+const add = ( var1, var2) =>  var1 + var2
