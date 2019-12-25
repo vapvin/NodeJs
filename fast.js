3437233cd1f29a4b95e2702281f23fca9ddf2e56
vapvin <tpehdj12@daum.net>
2019-12-25 12:58:50 +0900

    Repectory

--- fast.js
+++ fast.js
@@ -1,9 +1,8 @@
 'use strict'
 
-API.prototype.get = function(resource) {
-    var self = this;
-    return new Promise(function(resolve, reject){
-        http.get(self.uri + resource, function(data){
+API.prototype.get = (resource) => {
+     new Promise((resolve, reject) => {
+        http.get(this.uri + resource, data => {
             resolve(data);
         });
     });
