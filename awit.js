44b21303a5f8d38a68e70bb4d724ac954c9a5d75
vapvin <tpehdj12@daum.net>
2019-12-25 21:31:13 +0900

    Promise

---
+++ awit.js
@@ -0,0 +1,18 @@
+async function test() {
+    await asyncFunc(1,3000);
+    await asyncFunc(2,1000);
+    await asyncFunc(3,5000);
+}
+
+function asyncFunc(num, sec) {
+    return new Promise( (resolve, reject) => {
+        setTimeout(()=>{
+            console.log(num);
+            resolve('success')
+        }, sec);
+
+       
+    });
+}
+
+test();
