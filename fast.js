514946ca0cff95cfaacf5b06eb0d124130807038
vapvin <tpehdj12@daum.net>
2019-12-25 13:23:35 +0900

    Curried Function

--- fast.js
+++ fast.js
@@ -2,17 +2,8 @@
 
 // Curried Function
 
-const getDiscount = ( price, rate=0.1 ) => price * rate
+const getDiscount = rate => price => rate * price
 
+const getTen = getDiscount(0.1)
 
-const res = getDiscount(10000)
-const res2 = getDiscount(20000)
-const res3 = getDiscount(40000)
-const res4 = getDiscount(50000)
-const res5 = getDiscount(60000)
-
-console.log(res)
-console.log(res2)
-console.log(res3)
-console.log(res4)
-console.log(res5)
+getTen(price)
