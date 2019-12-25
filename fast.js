b4d455e3188880121b500b57156339ddd8855227
vapvin <tpehdj12@daum.net>
2019-12-25 13:12:48 +0900

    Too Hard..

--- fast.js
+++ fast.js
@@ -1,9 +1,18 @@
 'use strict'
 
-API.prototype.get = (resource) => {
-     new Promise((resolve, reject) => {
-        http.get(this.uri + resource, data => {
-            resolve(data);
-        });
-    });
-};
+// Curried Function
+
+const getDiscount = ( price, rate=0.1 ) => price * rate
+
+
+const res = getDiscount(10000)
+const res2 = getDiscount(20000)
+const res3 = getDiscount(40000)
+const res4 = getDiscount(50000)
+const res5 = getDiscount(60000)
+
+console.log(res)
+console.log(res2)
+console.log(res3)
+console.log(res4)
+console.log(res5)
