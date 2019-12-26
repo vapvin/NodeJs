b5454bdb2980eedcf437919fb16853ba5a5b2db6
vapvin <tpehdj12@daum.net>
2019-12-26 14:19:36 +0900

    Timer

--- fast.js
+++ fast.js
@@ -1,11 +1,17 @@
 'use strict'
 
-const promise1 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve(2000), 2000)
+const timeout = setTimeout(()=>{
+    console.log('first')
+}, 0)
+
+const immediate = setImmediate(()=>{
+    console.log('second')
 })
 
-const promise2 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('즉시'), 0)
-})
+const interval = setInterval(()=>{
+    console.log('third')
+}, 1000)
 
-Promise.race([ promise1, promise2]).then(value => console.log(value))
+clearTimeout(timeout)
+clearImmediate(immediate)
+clearInterval(interval)
