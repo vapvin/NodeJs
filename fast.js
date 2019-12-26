d7108c49275038d0954f6a1e85c16882c4723ae4
vapvin <tpehdj12@daum.net>
2019-12-26 13:39:07 +0900

    Promise Race

--- fast.js
+++ fast.js
@@ -1,10 +1,11 @@
 'use strict'
 
-const promise1 = new Promise((resolve, reject) => resolve('즉시호출'))
-
-const promise2 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('3초 뒤에 호출'), 3000)
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(2000), 2000)
 })
 
-Promise.all([promise1, promise2])
-.then(values => console.log(values))
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('즉시'), 0)
+})
+
+Promise.race([ promise1, promise2]).then(value => console.log(value))
