01b4be53cd8e993ed4924c9e1646534495a27be6
vapvin <tpehdj12@daum.net>
2019-12-28 00:46:34 +0900

    Error Handling

---
+++ error.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const CustomError = (message) => {
+    this.message = message
+    this.type = 'NotImageFileException'
+}
+
+try {
+    const imgTypes = ['.jpg', '.png', '.gif']
+    const filename = 'this.doc'
+
+    const isImageFile = imgTypes.find(ext => filename.endsWith(item))
+
+    if(!isImageFile) {
+        throw new CustomError('This is not an image file')
+    }
+
+} catch (e) {
+    console.error(e)
+}
