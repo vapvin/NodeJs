fea40ed7a5d7162a16ae834a909bb4387daf2e0e
vapvin <tpehdj12@daum.net>
2019-12-27 14:43:41 +0900

    Reduce 02

--- name.js
+++ name.js
@@ -1,14 +1,9 @@
 "use strict";
 
-const numbers = [10, 20, 30, 40];
+const numbers = [0, 1, 2, 3, 4, 5, 6];
+const res = numbers.reduce((tot, amt) => {
+  if (amt > 0) tot.push(amt);
+  return tot;
+}, []);
 
-const avg = numbers.reduce((tot, val, idx, arr) => {
-  tot += val;
-  if (idx === arr.length - 1) {
-    return tot / arr.length;
-  } else {
-    return tot;
-  }
-});
-
-console.log(avg);
+console.log(res);
