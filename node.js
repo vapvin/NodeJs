8992662ced8fceab4e4494f4673b4a1d94e568a6
vapvin <tpehdj12@daum.net>
2019-12-27 15:43:28 +0900

    asynchronous pattern02

--- node.js
+++ node.js
@@ -1,5 +1,22 @@
 "use strict";
 
 class DbManager {
-  constructor() {}
+  constructor(settings) {
+    this.settings = settings;
+    this.init = init;
+  }
+
+  query() {
+    // QUERY('') Agnostic
+  }
+
+  async init() {} // 최초 1회만 실행됩니다.
+
+  async newMember() {
+    await this.init;
+  }
+
+  async deleteMember() {
+    await this.init;
+  }
 }
