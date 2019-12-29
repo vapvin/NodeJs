ce55e457f2f353238c20896a8e21efb679dee8c0
vapvin <tpehdj12@daum.net>
2019-12-29 17:31:36 +0900

    Start Method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const http = require("http");
 const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 
 class ApiServer extends http.Server {
   constructor(config) {
@@ -15,7 +16,32 @@ class ApiServer extends http.Server {
     this.stopping = false;
   }
 
-  async start() {}
+  async start() {
+    this.app.use((req, res, next) => {
+      this.busy.add(req.socket);
+      res.on("finish", () => {
+        if (this.stopping) req.socket.end();
+        this.busy.delete(req.socket);
+      });
+      next();
+    });
+    this.app.use(cookieParser());
+
+    this.app.get("/_health", (req, res) => {
+      res.sendStatus(200);
+    });
+
+    this.app.use((err, req, res, next) => {
+      res.status(500).send(geerateApiError("Api::Error"));
+    });
+
+    this.on("connection", c => {
+      this.currentConns.add(c);
+      c.on("close", () => this.currentConns.delete(c));
+    });
+
+    return this;
+  }
 
   shutdown() {
     if (this.stopping) return;
@@ -26,7 +52,7 @@ class ApiServer extends http.Server {
 
     setTimeout(() => {
       console.error("비정상적인 종료 (강제종료)");
-      process.exit(0);
+      process.exit(1);
     }, this.config.shutdownTimeout).unref();
 
     if (this.currentConns.size > 0) {
