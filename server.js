54b339667e3631118df63b0bffad3971d983c2f5
vapvin <tpehdj12@daum.net>
2019-12-29 16:53:44 +0900

    Server Rebuilding

--- server.js
+++ server.js
@@ -1,40 +1,49 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const http = require('http')
-const helmet = require('helmet')
-const static = require('serve-static')
+const express = require("express");
+const http = require("http");
+const cookieParser = require("cookie-parser");
 
 class ApiServer extends http.Server {
-    constructor (config) {
-        const app = express()
-        super(app)
-        this.config = config
-        this.app = app
-        this.currentConns = new Set()
-        this.busy = new WeakSet()
-        this.stopping = false
-        this.app.static = static
-    }
+  constructor(config) {
+    const app = express();
+    super(app);
+    this.config = config;
+    this.app = app;
+    this.currentConns = new Set();
+    this.busy = new WeakSet();
+    this.stopping = false;
+  }
 
-    async start() {
-        this.app.use(helmet())
-        this.app.use(cookieParser())
-        this.app.use(bodyParser())
+  async start() {}
 
-        this.app.use(this.app.static(path.join(__dirname, 'dist'), {
-            setHeaders: (res, path) => {
-                res.setHeaders('Access-Control-Allow-Origin', '*')
-                res.setHeaders('Access-Control-Allow-Headers', '*')
-                res.setHeaders('Access-Control-Allow-Methods', 'GET')
-            }
-        }))
+  shutdown() {
+    if (this.stopping) return;
+    this.stopping = true;
+    this.close(() => {
+      process.exit(0);
+    });
+
+    setTimeout(() => {
+      console.error("비정상적인 종료 (강제종료)");
+      process.exit(0);
+    }, this.config.shutdownTimeout).unref();
+
+    if (this.currentConns.size > 0) {
+      console.log(
+        `현재 동시접속중인 연결(${this.currentConns.size})을 대기중입니다. `
+      );
+
+      for (const con of this.currentConns) {
+        if (!this.busy.has(con)) {
+          con.end();
+        }
+      }
     }
+  }
 }
 
 const init = async (config = {}) => {
-    const server = new ApiServer(config)
-    return server.start()
-}
+  const server = new ApiServer(config);
+  return server.start();
+};
